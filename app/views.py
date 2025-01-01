5514b7cfa1feea82e65f5170352486c0fe2cae36
Davi Leite <106539266+Davilt4@users.noreply.github.com>
2025-01-01 14:00:23 -0300

    feat: Implement geocoding function for addresses

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app.main import app
 from flask import render_template,url_for,jsonify,request,redirect
 from data.dados import porcentagem_bairros,data_com_mais_assaltos,data,carregar_dados_por_ano,data_adicionar_assalto,csv_to_dataframe
+from geocode.geocode import geocode_address
+
+API_KEY = "SUA_CHAVE_GOOGLE_API"
 
 @app.route("/")
 def home():
@@ -12,9 +15,26 @@ def estatisticas():
 
 @app.route('/adicionar_assalto', methods=['GET', 'POST'])
 def adicionar_assalto():
+    from geocode import geocode_address  # Importe a função
+API_KEY = "YOUR_GOOGLE_API_KEY"
+
+@app.route('/adicionar_assalto', methods=['GET', 'POST'])
+def adicionar_assalto():
     if request.method == 'POST':
-        data_adicionar_assalto(request.form['bairro'],request.form['latitude'],request.form['longitude'],request.form['data'])
-        return redirect(url_for('home'))
+        bairro = request.form['bairro']
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        data_extenso = request.form['data']
+
+        # Se latitude e longitude não forem fornecidos, tenta geocodificar
+        if not latitude or not longitude:
+            latitude, longitude = geocode_address(bairro, API_KEY)
+
+        if latitude and longitude:
+            data_adicionar_assalto(bairro, latitude, longitude, data_extenso)
+            return redirect(url_for('home'))
+        else:
+            return "Erro: Não foi possível determinar as coordenadas do endereço.", 400
     return render_template("adicionar_assalto.html")
 
 @app.route('/dados_json')
