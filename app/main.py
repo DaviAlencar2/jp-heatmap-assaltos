e1d29ee5948b3efb84b7fb4300c343290c362b99
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-29 19:47:37 -0300

    feat: initial config for django project

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__,template_folder="../templates",static_folder="../static")
-
-from app.views import *
-
-if __name__ == "__main__":
-    app.run() # commit teste
