fc21c24f504cc0a7fdead041a338422b6e51c6ab
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-31 16:17:37 -0300

    feat: criando funcoes para obter enderecos ou coordenadas

---
+++ heatmap/geocode/geocode.py
@@ -0,0 +1,41 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("GEOCODE_API_KEY")
+
+def geocode_address(address, API_KEY):
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {
+        "address": address,
+        "key": API_KEY
+    }
+    
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    
+    if data["status"] == "OK":
+        latitude = data["results"][0]["geometry"]["location"]["lat"]
+        longitude = data["results"][0]["geometry"]["location"]["lng"]
+        return latitude, longitude
+    else:
+        return None, None
+
+
+def coordinate_to_address(latitude, longitude, API_KEY):
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {
+        "latlng": f"{latitude},{longitude}",
+        "key": API_KEY
+    }
+    
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    
+    if data["status"] == "OK":
+        address = data["results"][0]["formatted_address"]
+        return address
+    else:
+        return None
