36c12b91663a764fbaf39805a153b6b29169dfcb
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-31 17:40:42 -0300

    test: versao rudimentar de rotas para dados

--- heatmap/urls.py
+++ heatmap/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, stats, add
+from .views import home, stats, add, assaltos, dados_ano
 
 urlpatterns = [
     path('', home, name = "home"),
     path('add/', add, name = "add" ),
     path('stats/', stats, name = 'stats'),
+    path('assaltos/', assaltos, name = 'assaltos'),
+    path('dados/<int:year>/', dados_ano, name = 'dados_ano' ), 
 ]
--- heatmap/views.py
+++ heatmap/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Assalto
+from datetime import datetime
 
 def home(request):
     return render(request, "heatmap/home.html", {'show_year_dropdown': True})
@@ -8,3 +11,35 @@ def stats(request):
 
 def add(request):
     return render(request, "heatmap/add.html")
+
+# Eu sei que essa implementacao é ruim, irei melhorar com formulario e autorizaçao
+def assaltos(request):
+    assaltos_list = Assalto.objects.all()
+    data = []
+    for assalto in assaltos_list:
+        data.append({
+            'id': assalto.id,
+            'latitude': assalto.latitude,
+            'longitude': assalto.longitude,
+            'data': assalto.date.strftime('%d/%m/%Y'),
+            'hora': assalto.time.strftime('%H:%M'),
+            'descricao': assalto.description,
+            'localizacao': assalto.location
+        })
+    return JsonResponse(data, safe=False)
+
+def dados_ano(request, year):
+    assaltos_list = Assalto.objects.filter(date__year=year)
+    data = []
+    for assalto in assaltos_list:
+        data.append({
+            'id': assalto.id,
+            'Latitude': str(assalto.latitude),
+            'Longitude': str(assalto.longitude),
+            'Data': assalto.date.strftime('%d/%m/%Y'),
+            'Hora': assalto.time.strftime('%H:%M'),
+            'Descricao': assalto.description,
+            'Bairro': assalto.location
+        })
+    return JsonResponse(data, safe=False)
+
