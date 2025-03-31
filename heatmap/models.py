86f4026d37510cf84dec1401078a832d12ce4518
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-31 13:51:14 -0300

    feat: criando tabela de assaltos e alterando atributos

--- heatmap/models.py
+++ heatmap/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Assalt(models.Model):
-    latitude = models.CharField(max_length=255)
-    longitude = models.CharField(max_length=255)
+class Assalto(models.Model):
+    latitude = models.CharField(max_length=255, blank=True, null=False)
+    longitude = models.CharField(max_length=255, blank=True, null=False)
     date = models.DateField()
     time = models.TimeField()
     description = models.TextField(blank=True, null=True)
-    location = models.CharField(max_length=255)
+    location = models.TextField()
 
     def __str__(self):
-        return f"Assalto em: {self.location}, na data: {self.date} as: {self.time}"
+        return f"Assalto em: {self.location}, na data: {self.date}, às: {self.time}"
