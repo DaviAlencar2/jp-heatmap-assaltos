9c21f26129e854dece47b2cc5ea8257c9133fb7d
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-04-01 11:39:20 -0300

    feat: configurando admin para os assaltos

--- heatmap/admin.py
+++ heatmap/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Assalto
 
-# Register your models here.
+class ListarAssaltos(admin.ModelAdmin):
+    list_display = ("id","location","date","time")
+    list_display_links = ("id","location")
+    search_fields = ("date","location")
+
+admin.site.register(Assalto,ListarAssaltos)
--- heatmap/models.py
+++ heatmap/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+MODEL_LOCATION_EXAMPLE = "RUA, N° - BAIRRO, CIDADE - SIGLA_ESTADO, CEP, PAIS"
+
 class Assalto(models.Model):
+    TYPES = [("a","assalto"),
+             ("f","furto")
+    ]
+
+
     latitude = models.CharField(max_length=255, blank=True, null=False)
     longitude = models.CharField(max_length=255, blank=True, null=False)
     date = models.DateField()
     time = models.TimeField()
     description = models.TextField(blank=True, null=True)
     location = models.TextField()
+    type = models.CharField(max_length=1,blank=False, null=False, choices=TYPES, default="a")
+
 
     def __str__(self):
         return f"Assalto em: {self.location}, na data: {self.date}, às: {self.time}"
