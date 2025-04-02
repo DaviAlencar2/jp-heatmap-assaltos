a98659a7f43ea987d4a6c33fe1adc1ee03cd9417
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-04-02 09:24:43 -0300

    refactor: remover endpoint de assaltos

--- heatmap/urls.py
+++ heatmap/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, stats, add, assaltos, dados_ano
+from .views import home, stats, add, data_by_year
 
 urlpatterns = [
     path('', home, name = "home"),
     path('add/', add, name = "add" ),
     path('stats/', stats, name = 'stats'),
-    path('assaltos/', assaltos, name = 'assaltos'),
-    path('dados/<int:year>/', dados_ano, name = 'dados_ano' ), 
+    path('dados/<int:year>/', data_by_year, name = 'dados_ano' ), 
 ]
--- heatmap/views.py
+++ heatmap/views.py
@@ -14,36 +14,21 @@ def add(request):
     return render(request, "heatmap/add.html")
 
 # Eu sei que essa implementacao é ruim, irei melhorar com formulario e autorizaçao
-def assaltos(request):
-    assaltos_list = Robbery.objects.all()
-    data = []
-    for assalto in assaltos_list:
-        data.append({
-            'id': assalto.id,
-            'latitude': assalto.latitude,
-            'longitude': assalto.longitude,
-            'data': assalto.date.strftime('%d/%m/%Y'),
-            'hora': assalto.time.strftime('%H:%M'),
-            'descricao': assalto.description,
-            'localizacao': assalto.location.neighborhood.name,
-        })
-    return JsonResponse(data, safe=False)
 
-def dados_ano(request, year):
-    assaltos_list = Robbery.objects.filter(date__year=year)
+def data_by_year(request, year):
+    robberies_list = Robbery.objects.filter(date__year=year)
     data = []
-    for assalto in assaltos_list:
-        if assalto.location.latitude == "" or assalto.location.longitude == "":
-            location = f"{assalto.location.street}, {assalto.location.number} - {assalto.location.neighborhood.name}"
+    for robbery in robberies_list:
+        if robbery.location.latitude == "" or robbery.location.longitude == "":
+            location = f"{robbery.location.street}, {robbery.location.number} - {robbery.location.neighborhood.name}"
         latitude, longitude = geocode_address(location)
         data.append({
-            'id': assalto.id,
+            'id': robbery.id,
             'Latitude': str(latitude),
             'Longitude': str(longitude),
-            'Data': assalto.date.strftime('%d/%m/%Y'),
-            'Hora': assalto.time.strftime('%H:%M'),
-            'Descricao': assalto.description,
+            'Data': robbery.date.strftime('%d/%m/%Y'),
+            'Hora': robbery.time.strftime('%H:%M'),
+            'Descricao': robbery.description,
             'localizacao': location,
         })
     return JsonResponse(data, safe=False)
-
