9b0d7591734091f09e32f22a6e82bf2018bdeff9
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-04-11 07:45:02 -0300

    refactor: mudando identacao de alguns arquivos

--- heatmap/models.py
+++ heatmap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Neighborhood(models.Model):
     name = models.CharField(max_length=100, null=False)
@@ -6,21 +7,32 @@ class Neighborhood(models.Model):
     def __str__(self):
         return self.name
 
+
 class Robbery(models.Model):
     TYPES = [("a","assalto"),
              ("f","furto")
     ]
 
-    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name="robberies",default="")
+    neighborhood = models.ForeignKey(
+        Neighborhood, on_delete=models.CASCADE, related_name="robberies",default="")
+    
     date = models.DateField()
     time = models.TimeField()
+
     description = models.TextField(blank=True, null=True)
+
     street = models.CharField(max_length=255, blank=True, null=False)
     number = models.CharField(max_length=255, blank=True, null=False)
     latitude = models.CharField(max_length=255, blank=True, null=False)
     longitude = models.CharField(max_length=255, blank=True, null=False)
+
     type = models.CharField(max_length=1,blank=False, null=False, choices=TYPES, default="a")
     is_valid = models.BooleanField(default=False)
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="robberies", default="")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"Assalto em: {self.neighborhood.name}, as: {self.date} - {self.time}"
