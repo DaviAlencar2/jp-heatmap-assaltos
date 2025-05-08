6c4eccec2a09aa2e1e93dfab1136aa2f7b88dfed
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-05-08 09:20:59 -0300

    feat: migrando bd para supabase

--- setup/settings.py
+++ setup/settings.py
@@ -70,35 +70,14 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if os.getenv('RENDER_DATABASE_EXTERNAL_URL'):
-    # Configuração manual do PostgreSQL com variáveis de ambiente
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
-elif os.getenv('RENDER_DATABASE_INTERNAL_URL'):
-    # Configuração automática do PostgreSQL com dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('RENDER_DATABASE_INTERNAL_URL')
-        )
-    }
-
-else: # Uso desencorajado ja que os dados vao estar no render.
-    # Configuração para desenvolvimento local com SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
